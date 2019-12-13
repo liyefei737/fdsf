43471ac1ed050367520b703d640741eac02aa5a5
unknown <macrofly@gmail.com>
2019-12-12 20:30:45 -0800

    add menu component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-//import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap'
-import './App.css';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
+import Menu from "./components/MenuComponent";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navbar dark color='primary'>
-      <div className='container'>
-      <NavbarBrand href='/'>Pad Thai</NavbarBrand>
-      </div>
-      </Navbar>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Menu</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
       </div>
     );
   }
