099c210b98f6183b0da0a62816f1dbb45a032b78
unknown <macrofly@gmail.com>
2019-12-13 09:03:19 -0800

    Seprate out App state and menu state & redesign menu comp using cards

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
-import "./App.css";
+//import "./App.css";
 import Menu from "./components/MenuComponent";
+import Dishes from './dishes'
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      dishes: Dishes
+    }
+  }
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Menu</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
