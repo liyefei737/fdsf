4e63f2bc81223825fcd769616a8b69e16d9d17ce
unknown <macrofly@gmail.com>
2019-12-13 16:56:07 -0800

    format DishdetailComponent.js

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle
-} from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 class Dishdetail extends Component {
-
   renderDish(dish) {
     return (
       <Card>
@@ -22,22 +15,30 @@ class Dishdetail extends Component {
   }
 
   renderComments(comments) {
-    const commentsList = comments.map((c) => (<li key={c.id}><span>{c.comment}</span>
-                                <span>{c.author}    {c.date}</span></li>));
+    const commentsList = comments.map(c => (
+      <li key={c.id}>
+        <span>{c.comment}</span>
+        <span>
+          {c.author} {c.date}
+        </span>
+      </li>
+    ));
     return commentsList;
   }
 
   render() {
     const dish = this.props.dish;
     if (dish === null) {
-      return <div></div>
+      return <div></div>;
     }
-    return [<div className="col-12 col-md-5">{this.renderDish(dish)}</div>,
-           <div className="col-12 col-md-5">
-           <h4> Comments </h4>
+    return [
+      <div className="col-12 col-md-5">{this.renderDish(dish)}</div>,
+      <div className="col-12 col-md-5">
+        <h4> Comments </h4>
 
-           <ul className="list-unstyled">{this.renderComments(dish.comments)}</ul>
-           </div>];
+        <ul className="list-unstyled">{this.renderComments(dish.comments)}</ul>
+      </div>
+    ];
   }
 }
 
