d1d6fe49d9e5533503e5ec17629330de12ff57c7
unknown <macrofly@gmail.com>
2019-12-15 12:25:38 -0800

    add component router to app

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,37 +3,30 @@ import Menu from "./MenuComponent";
 import Dishdetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
 import Dishes from "../dishes";
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: Dishes,
-      selectedDishID: null
     };
   }
 
-  onDishSelect(dishID) {
-    this.setState({ selectedDishID: dishID });
-  }
-
   render() {
+    const HomePage = () => {
+        return (<Home />);
+    }
     return (
       <div>
         <Header />
-        <Menu
-          dishes={this.state.dishes}
-          selectedDishHandler={selectedDishID => {
-            this.onDishSelect(selectedDishID);
-          }}
-        />
-
-        <Dishdetail
-          dish={this.state.dishes
-            .filter(d => d.id === this.state.selectedDishID)
-            .pop()}
-        />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} /> } />
+          <Redirect to='/' />
+        </Switch>
         <Footer/ >
       </div>
     );
